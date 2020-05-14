1a85934544173b2f2b3f4ca02bb998e99ecdadff
Renan Ribeiro <63807288+renanribs@users.noreply.github.com>
2020-05-14 13:12:56 -0300

    Update
    Resultado teste 100%
    Formatação PEP8

--- main.py
+++ main.py
@@ -1,82 +1,72 @@
 from datetime import datetime
 
 records = [
-    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
-    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
-    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
-    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
-    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
-    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
-    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000},
+    {'source': '48-996355555', 'destination': '48-666666666',
+     'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097',
+     'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097',
+     'end': 1564630198, 'start': 1564629838},
+    {'source': '48-999999999', 'destination': '41-885633788',
+     'end': 1564697158, 'start': 1564696258},
+    {'source': '41-833333333', 'destination': '41-885633788',
+     'end': 1564707276, 'start': 1564704317},
+    {'source': '41-886383097', 'destination': '48-996384099',
+     'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697',
+     'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099',
+     'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697',
+     'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097',
+     'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788',
+     'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788',
+     'end': 1564627800, 'start': 1564626000},
 ]
 
 
-def calcula_segundos():
-    for record in records:
-        segundosini = datetime.fromtimestamp(record['start'])
-        segundosfin = datetime.fromtimestamp(record['end'])
-        segundos = segundosfin.second - segundosini.second
-        return segundos
-
-
-def calcula_minutos():
-    for record in records:
-        start = datetime.fromtimestamp(record['start'])
-        end = datetime.fromtimestamp((record['end']))
-        minutostotal = end.minute - start.minute
-        return minutostotal
-
-
-def calcula_hora_inicio():
-    for record in records:
-        start = datetime.fromtimestamp(record['start'])
-        hora_inicio = start.hour
-        return hora_inicio
-
-
-def calcula_hora_final():
-    for record in records:
-        end = datetime.fromtimestamp(record['end'])
-        hora_final = end.hour
-        return hora_final
-
-
-def calcula_preco():
-    preco_total = 0
-    horaInicio = calcula_hora_inicio()
-    horaFinal = calcula_hora_final()
-    totalsegundos = calcula_segundos()
-    totalminutos = calcula_minutos()
-    taxa_fixa = 0.36
-    taxa_minuto = 0.09
-
-    if horaInicio >= 6:
-        if horaFinal <= 21 and totalminutos <= 59:
-            preco_total = totalsegundos * taxa_minuto + taxa_fixa
-
-        if horaFinal >= 22 and totalminutos > 0:
-            preco_total += taxa_fixa
-    else:
-        preco_total = taxa_fixa
-    return preco_total
-
-
 def classify_by_phone_number(records):
-    ligacoes = []
+    dicionario = {}
+    lista = []
+
     for record in records:
-        lista = next((i for i in range(len(ligacoes)) if ligacoes[i]['source'] == record['source']), -1)
-        valor = calcula_preco()
-        if lista == -1:
-            ligacoes.append({'source': record['source'], 'total': valor})
+        horaInicio = datetime.fromtimestamp(record['start'])
+        horaFinal = datetime.fromtimestamp((record['end']))
+
+        if record['source'] not in dicionario.keys():
+            dicionario[record['source']] = calcula_preco(horaInicio,
+                                                         horaFinal)
+
         else:
-            ligacoes[lista]['total'] = round(valor + ligacoes[lista]['total'], 2)
-    ligacoes.sort(key=lambda k: k['total'], reverse=True)
-    return ligacoes
+            dicionario[record['source']] += calcula_preco(horaInicio,
+                                                          horaFinal)
+
+    for key, value in dicionario.items():
+        lista.append({'source': key, 'total': round(value, 2)})
+
+    lista = sorted(lista, key=lambda item: item['total'], reverse=True)
+
+    return lista
 
 
-print(classify_by_phone_number(records))
+def calcula_preco(horaInicio, horaFinal):
+    taxa_minuto = 0.09
+    taxa_fixa = 0.36
+    total = 0
+
+    if 6 <= horaInicio.hour < 22:
+        if horaFinal.hour < 22:
+            hora_total = (horaFinal - horaInicio)
+            total += taxa_fixa + (hora_total.seconds // 60 % 60) * taxa_minuto
+        else:
+            horaFinal = datetime(horaInicio.year, horaInicio.month,
+                                 horaInicio.day, 22)
+            hora_total = horaFinal - horaInicio
+            total += (((hora_total.seconds // 60 % 60) * taxa_minuto)
+                      + taxa_fixa)
+    else:
+        total += taxa_fixa
+    return total
